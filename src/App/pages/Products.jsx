65a739e57f2710dba4b570144f2bf9525c0c81f7
RodrigoSpano <rorrospano17@gmail.com>
2022-06-08 19:37:58 -0300

    .

--- src/App/pages/Products.jsx
+++ src/App/pages/Products.jsx
@@ -8,7 +8,7 @@ export default function Products() {
   return (
     <>
     <Stack bgImage={bannerProds} w='100vw' h='md' justifyContent='center' alignItems='center'>
-      <Heading as='h2' fontSize='9xl' color='blue.500'>SNEAKERS</Heading>
+      <Heading as='h2' fontSize='9xl' color='blue.500' userSelect='none'>SNEAKERS</Heading>
     </Stack>
     <Stack minH='100vh' w='100vw' mt={20} direction='row' >
       <>
