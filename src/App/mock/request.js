d9db2e0020005675f90b2ec77aeda4a853f122ff
RodrigoSpano <rorrospano17@gmail.com>
2022-06-08 15:50:07 -0300

    first commit en esta branch

--- src/App/mock/request.js
+++ src/App/mock/request.js
@@ -1,9 +1,18 @@
 import baseMostPopular  from "./data";
+import dbProducts from "./db";
 
 export const requestPopular  = () => {
   return new Promise((resolve, reject) => {
     setTimeout(()=>{
       resolve(baseMostPopular)
+    }, 3000)
+  })
+}
+
+export const requestAll = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      resolve(dbProducts)
     }, 3000)
   })
 }
