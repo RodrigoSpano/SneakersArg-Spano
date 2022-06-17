f8ea1d5ecdb7881bfae41e5a55b960f49778ba45
RodrigoSpano <rorrospano17@gmail.com>
2022-06-16 22:07:27 -0300

    elimine carpeta mock para usar firebase

--- src/App/mock/request.js
+++
@@ -1,9 +0,0 @@
-import dbProducts from "./db";
-
-export const requestAll = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(()=>{
-      resolve(dbProducts)
-    }, 1000)
-  })
-}
