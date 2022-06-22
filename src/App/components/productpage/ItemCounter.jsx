d0b94a1c35533e9064ee684c7addc83a5696283e
RodrigoSpano <rorrospano17@gmail.com>
2022-06-22 20:31:58 -0300

    .

--- src/App/components/productpage/ItemCounter.jsx
+++ src/App/components/productpage/ItemCounter.jsx
@@ -14,13 +14,13 @@ const ItemCounter = ({counter, setCounter, max, handleAdd, size, stock }) => {
 
   return(
     <Flex direction='column' maxW='sm' gap={5}>
-      <Text fontFamily='open sans' > Available Stock: {stock} unit..</Text>
+      <Text fontFamily='open sans' > {stock > 0 ? `Available Stock: ${stock} unit..` : 'INSUFICIENT STOCK'}</Text>
       <Text fontFamily='open sans' >Availables sizes: {size}</Text>
       <Box display='flex' flexDirection='row' alignItems='center' gap={2} maxW='sm'>
         <Button onClick={handleDecrease} variant='inc' >-</Button>
         <chakra.span fontSize='30px' fontFamily='Open Sans' >{counter}</chakra.span>
         <Button onClick={handleIncrease} variant='inc' >+</Button>
-        <Button onClick={handleAdd} variant='cart'>Add to cart</Button>
+        <Button onClick={handleAdd} disabled={stock <= 0} variant='cart'>Add to cart</Button>
       </Box>
 
     </Flex>
